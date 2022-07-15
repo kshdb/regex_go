79f15e7b926dcd6438c57d5a420430a6690ddc86
sxh <123456>
2022-07-15 15:33:04 +0800

    屏蔽idea

--- demo/main.go
+++ demo/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-   "github.com/kshdb/regex_go"
+	regex "github.com/kshdb/regex_go"
 )
 
 func main() {
 	//fmt.Println("测试")
 	_str := `[{"AAA":"1111","BBB":"222","CCC":""333 444"","DDD":"555"},{"AAA":"1111","BBB":"222","CCC":""333 444"","DDD":"555"},{"AAA":"1111","BBB":"222","CCC":""333 444"","DDD":"555"},{"AAA":"1111","BBB":""333 444"","CCC":"555","DDD":""}]`
-	reg := regex_go.MustCompile(`\"\"`, regex_go.RE2)
+	reg := regex.MustCompile(`\"\"`, regex.RE2)
 	m, _err := reg.Replace(_str, `"`, 0, -1)
 	fmt.Println("执行的结果是", m, _err)
 
@@ -18,14 +18,14 @@ hooeeoooeeeeeen.com
 www.oo234r44.ss.com 
 hoo666666.com
 	`
-	reg1 := regex_go.MustCompile(`(?<=o{2})([2-6a-z\.]+)(?=\.)`, regex_go.RE2)
+	reg1 := regex.MustCompile(`(?<=o{2})([2-6a-z\.]+)(?=\.)`, regex.RE2)
 	//sss, _ := reg1.FindStringMatch(_srt1) //获取单个
 	sss := regexp2FindAllString(reg1, _srt1) //获取批量
 	fmt.Println("执行的结果是", sss)
 
 }
 
-func regexp2FindAllString(re *regex_go.Regexp, s string) []string {
+func regexp2FindAllString(re *regex.Regexp, s string) []string {
 	var matches []string
 	m, _ := re.FindStringMatch(s)
 	for m != nil {
